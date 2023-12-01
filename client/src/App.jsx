b633d548cdb1279f771f2193a3e6ffdcb650e140
Karim <karem109k@gmail.com>
2023-12-01 14:52:59 +0200

    Routes And Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
+import { IoSearch } from "react-icons/io5";
+import { IoMdMenu } from "react-icons/io";
+import { FaXmark } from "react-icons/fa6";
+
+
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <div className='bg-slate-100 '>
+      <div className='flex justify-between items-center gap-3 max-w-6xl p-3 mx-auto'>
+        <Link className='flex justify-center' to='/'><img src={logo} alt="Logo" className='w-[100px]' /></Link>
+
+        <div className="lg:hidden" onClick={() => setShowMenu(true)}>
+          <IoMdMenu className='text-2xl' />
+        </div>
+
+        {showMenu && <MobileSlider setShowMenu={setShowMenu} />}
+
+        <div className='hidden lg:flex justify-center items-center relative'>
+          <input type="text" className='p-2 pr-10 rounded outline-none w-[350px]' />
+          <IoSearch className='absolute top-1/2 -translate-y-1/2 right-2' />
+        </div>
+        <div className='hidden lg:flex justify-center gap-4 font-medium'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/login'>Login</Link>
+          <Link to='/signup'>Sign Up</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const MobileSlider = ({setShowMenu}) => {
+  const [open,setOpen] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setOpen(true), 0);
+  })
+  return (
+    <>
+      <div onClick={() => setShowMenu(false)} className='fixed top-0 right-0 h-screen w-screen bg-[#00000033]'></div>
+      <div style={{right: open ? "0%" : "-100%"}} className='h-screen bg-white w-[80%] flex flex-col items-end gap-5 p-5 z-10 fixed top-0 right-0 duration-300'>
+
+        <div onClick={() => setShowMenu(false)} className='bg-slate-100 p-2 rounded w-fit'>
+          <FaXmark />
+        </div>
+
+        <div className='relative w-full'>
+          <input type="text" className='bg-slate-50 w-full p-2 rounded border outline-none text-left pr-10' />
+          <IoSearch className='absolute top-1/2 -translate-y-1/2 right-2' />
+        </div>
+
+        <div className='flex flex-col w-full'>
+          <Link className=' p-3 border-b w-full' to='/'>Home</Link>
+          <Link className=' p-3 border-b w-full' to='/about'>About</Link>
+          <Link className=' p-3 border-b w-full' to='/login'>Login</Link>
+          <Link className=' p-3 border-b w-full' to='/signup'>Sign Up</Link>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Header
